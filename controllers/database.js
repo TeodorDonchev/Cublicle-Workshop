d0cf82784915bddaebde8547e2a7aee797b18065
TeodorDonchev <teodorgdo@gmail.com>
2020-06-17 02:48:55 +0300

    feat: made app work with mogodb with database models

--- controllers/database.js
+++ controllers/database.js
@@ -1,46 +1,17 @@
-const fs = require('fs');
-const path = require('path');
+const Cube = require('../models/cube')
 
-const database = path.join(__dirname, '..', 'config/database.json');
-
-const saveCube = (cube, callback) => {
-
-    getAllCubes((cubes) => {
-        cubes.push(cube);
-
-        fs.writeFile(database, JSON.stringify(cubes), error => {
-            if (error) {
-                throw error;
-            }
-            console.log('Cube is succesfully stored.');
-        });
-
-        callback();
-    })
+const getCube = async function (id) {
+    const cube = await Cube.findById(id).lean();
+    return cube;
 }
 
-const getCube = (id, callback) => {
-    getAllCubes((cubes) => {
-        const cube = cubes.filter(c => c.id === id)[0];
-
-        callback(cube);
-    });
-}
-
-const getAllCubes = (callback) => {
-    fs.readFile(database, (err, data) => {
-        if (err) {
-            throw err;
-        }
-
-        const cubes = JSON.parse(data);
-        callback(cubes);
-    });
+const getAllCubes =  async function() {
+    const cubes = await Cube.find().lean();
+    return cubes;
 }
 
 
 module.exports = {
-    saveCube,
     getCube,
     getAllCubes,
 }
