3892e267601627b1ba08c1ca08f04ab48b9e03f6
TeodorDonchev <teodorgdo@gmail.com>
2020-06-20 02:55:31 +0300

    fea: added creatorID for cube

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     development: {
         port: process.env.PORT || 3000,
+        privateKey: 'CUBE_WORKSHOP_SOFTUNI',
         databaseUrl: `mongodb+srv://cubicle:${process.env.DB_PASSWORD}@cubicle-6tvqn.mongodb.net/cubicledb?retryWrites=true&w=majority`
     },
     production: {}
